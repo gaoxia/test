4e16599cd3249301028bec8b3a3402fd924cc4ed
gaoxia <gaoxia_sdus@126.com>
2014-09-13 01:24:38 +0800

    modify 1.c add 2.c

--- 2.c
+++ 2.c
@@ -1,19 +1,22 @@
 /*************************************************************************
-	> File Name: 1.c
+	> File Name: 2.c
 	> Author: gx
 	> Mail:178629574@qq.com 
-	> Created Time: Fri 12 Sep 2014 08:10:16 PM CST
+	> Created Time: Sat 13 Sep 2014 01:23:28 AM CST
  ************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 int main(int argc, char* argv[])
 {
-	printf("hello world!\n");
-	printf("hello world!\n");
-
-	printf("xxxxx");
+	printf("aasdfasdf");
     return 0;
 }
