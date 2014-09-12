abdaa43ece6224e6c9654e220fcd5862b45482ab
gaoxia <gaoxia_sdus@126.com>
2014-09-12 20:24:23 +0800

    xxxx

---
+++ 1.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: gx
+	> Mail:178629574@qq.com 
+	> Created Time: Fri 12 Sep 2014 08:10:16 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	printf("hello world!\n");
+    return 0;
+}
